375aaf50b07441de899b09b67c6272aa3caa049d
Mostafa <mostafa@>
2024-01-21 17:58:46 +0200

    finish forget pass screens ui

---
+++ lib/ui/view/home_screen/category_screen/categorylist.dart
@@ -0,0 +1,9 @@
+class Categoryl  {
+ String title='';
+ String imagepath='';
+
+ Categoryl({required this.title,required this.imagepath});
+static List<Categoryl> categrylist(){
+
+}
+}
---
+++ lib/ui/view/home_screen/master_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Masterscreen extends StatefulWidget {
+  const Masterscreen({super.key});
+  static const String mastername = 'mastername';
+
+  @override
+  State<Masterscreen> createState() => _MasterscreenState();
+}
+
+class _MasterscreenState extends State<Masterscreen> {
+  int currentindex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Color(0xff8362D7),
+        unselectedItemColor:  Color(0xffC8C8C8),
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/home.png')),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/insights.png')),
+            label: 'Insights',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/todoicon.png')),
+            label: 'To do list',
+          ),
+        ],
+        onTap: (index){
+          currentindex = index;
+          setState(() {});
+        },
+        currentIndex: currentindex,
+      ),
+      backgroundColor: Colors.white,
+      body: pages[currentindex],
+    );
+  }
+}
+List pages =[];
